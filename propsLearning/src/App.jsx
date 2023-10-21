7a600966958df78b2334270cac9178161c6a14f6
Tamish <tamishbajaj360@gmail.com>
2023-10-21 20:26:38 +0530

    basic props learning project

---
+++ propsLearning/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Card from './components/Card';
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-2xl'>TaliwindTest</h1>
+      <Card  username='Tamish' />
+      <Card  username='Abd' />
+
+
+    </>
+  )
+}
+
+export default App
